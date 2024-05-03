6e39bc7ccbef0bbab71c206cba085d736dc209b9
RonnieEllz <bsc-inf-12-21@unima.ac.mw>
2024-05-03 17:29:05 +0200

    Today's lab assignment

---
+++ Lab7/PointersExer.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+    //This functionis to swap the two intergers using the & references
+void swapNumbers(int* a,int* b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+int main(){
+    int varA = 25;
+    int varB =100;
+
+    cout<<"varA before swap:" <<varA <<endl;
+    cout<<"varB before swap:" <<varB <<endl;
+        swapNumbers(&varA,&varB);
+    cout<< "varA after swap:"<<varA<<endl;
+    cout<< "varB after swap:"<<varB<<endl;
+    
+    return 0;
+}
